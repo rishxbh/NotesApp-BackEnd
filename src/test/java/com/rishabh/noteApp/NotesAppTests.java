80475f14f69a1f6ab52d4c15721452d5b29b6a04
rishxbh <rishabh112000@gmail.com>
2024-01-08 01:26:18 +0530

    worked on all apis

---
+++ src/test/java/com/rishabh/noteApp/NotesAppTests.java
@@ -0,0 +1,13 @@
+package com.rishabh.noteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
